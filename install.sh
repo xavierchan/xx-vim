32c410a039e7f4b00bfe9f05bc8aea6be1c9ed59
xavierchan <xavierchan@chenzhuoxians-MacBook-Pro.local>
2019-04-23 22:54:28 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 if [ -f "~/.vimrc" ]; then
-    cp ~/.vimrc ~/.vimrc.back
+    cp "~/.vimrc" "~/.vimrc.back"
 fi
-if [ ! -d "~/.vim/bundle"]
-    mkdir ~/.vim/bundle
+if [ ! -d "~/.vim/bundle"]; then
+    mkdir "~/.vim/bundle"
 fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-wget https://github.com/xavierchan/xx-vim/blob/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/xavierchan/xx-vim/master/.vimrc -O ~/.vimrc
